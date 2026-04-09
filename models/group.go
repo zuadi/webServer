72d850d74f18f496e1866e4238d6695c61853f92
Adrian Zürcher <zuercher@mst.local>
2026-04-09 07:33:30 +0200

    fix group path

--- models/group.go
+++ models/group.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/zuadi/webServer/logger"
 	"github.com/zuadi/webServer/utils"
 )
 
@@ -11,15 +12,21 @@ type Group struct {
 
 func (g *Group) Group(path string) *Group {
 	return &Group{
-		Path:  g.Path + "/" + utils.CleanPath(path),
+		Path:  utils.CleanPath(g.Path) + utils.CleanPath(path),
 		Route: g.Route,
 	}
 }
 
 func (g *Group) Get(path string, handler Handler) {
-	g.Route.Insert("GET", g.Path+"/"+utils.CleanPath(path), handler)
+	title := "GET"
+	path = utils.CleanPath(g.Path) + utils.CleanPath(path)
+	logger.SetStyle(title, "#56a7f8", path)
+	g.Route.Insert(title, path, handler)
 }
 
 func (g *Group) Post(path string, handler Handler) {
-	g.Route.Insert("Post", g.Path+"/"+utils.CleanPath(path), handler)
+	title := "POST"
+	path = utils.CleanPath(g.Path) + utils.CleanPath(path)
+	logger.SetStyle(title, "#56a7f8", path)
+	g.Route.Insert(title, path, handler)
 }
